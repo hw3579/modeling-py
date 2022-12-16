f9f3817a925026e3cedb2de39a4fd579cf8f5af1
hw3579 <1191222076@qq.com>
2022-12-16 19:02:28 +0000

    19.02

--- sub.py
+++ sub.py
@@ -1,33 +1,60 @@
+'''
+# Course: Modeling of Engineering
+# Author: Jiaqi Yao
+# Date: Dec 16, 2022
+'''
+
+# Import modules and library functions
 import numpy as np
 from scipy import integrate
 from matplotlib import pyplot as plt
 
-
-
-
-#h=float(input("step size:"))   #define the step size
-h=0.1 #for test
-print("h=")
+'''
+Step 1: input the step size h
+'''
+#h=float(input("step size:"))   # input step size manually
+h=0.01                          # choose one method to set the step size
+print("h=")                     # print the step size
 print(h)
 
-x=np.arange(0,45,h) # each of the time point
+
+'''
+Step 2: Defind basic variables and functions
+'''
+
+x=np.arange(0,45,h) # Selecting time sampling points, from 0 to 45 in each h
 
 
-def impulse(t):
+def impulse(t):     # Define the impulse force
     if t<1:
         return 1
     else:
         return 0
 
-def Euler(Z,x): #function for Euler method
-    Z = np.zeros((len(x), 4))
-    Z3=np.zeros(len(x))
-    for i in range(len(x)-1):
-        Z[i+1]=h*state_matrix(x[i], Z[i])+Z[i]
-        Z3[i]=Z[i][2]
-    
-    return Z3
 
+def state_matrix(x,Z): # Define the state space matrix
+ dz1=Z[1]
+ dz2=-Z[0]-Z[1]+Z[3]    # There is a -0.5 be ignored because the constant disappears after differentiation.
+ dz3=Z[3]
+ dz4=Z[1]-Z[3]+0.5*impulse(x)
+
+ return np.array([dz1,dz2,dz3,dz4])
+
+
+
+
+def Euler(Z,x):     # function for Euler method
+    Z = np.zeros((len(x), 4))   # Defining two-dimensional arrays Z: 
+    Z3=np.zeros(len(x))         # Defining one-dimensional arrays Z3 for extract the X2(t)
+    for i in range(len(x)-1):
+        Z[i+1]=h*state_matrix(x[i], Z[i])+Z[i] # f(x)=f(x,y)+h*f'(x,y)
+        Z3[i]=Z[i][2]      # Extract the X2(t)
+    
+    return Z3   # Return a one-dimensional arrays
+
+
+
+# This is the old version of function by using one-dimensional arrays
 '''
     for i in range(len(x)-1):
         z1[i+1]=z1[i]+h*f1(z2[i],x[i])   
@@ -39,14 +66,17 @@ def Euler(Z,x): #function for Euler method
     return [z1,z2,z3,z4]
 '''
 
-def heun(Z,x): #function for Heun method
+def heun(Z,x):      #  function for Heun method
     Z = np.zeros((len(x), 4))
     Z3=np.zeros(len(x))
     for i in range(len(x)-1):
-        Z[i+1]=Z[i]+h*(0.5*state_matrix(x[i], Z[i])+0.5*state_matrix(x[i]+h, Z[i]+h*state_matrix(x[i], Z[i])))
+        Z[i+1]=Z[i]+h*(0.5*state_matrix(x[i], Z[i])+0.5*state_matrix(x[i]+h, Z[i]+h*state_matrix(x[i], Z[i]))) 
+        #k1=f(x,y)
+        #k2=f(x+h,y+k1*h)
+        # f(x+1)=f(x,y)+h*(0.5*k1+0.5*k2)
         Z3[i]=Z[i][2]
     
-    return Z3
+    return Z3  
 
 '''
     h=x[1]-x[0]
@@ -66,13 +96,10 @@ def heun(Z,x): #function for Heun method
 '''
 
 
-def state_matrix(x,Z):
- dz1=Z[1]
- dz2=-Z[0]-Z[1]+Z[3]
- dz3=Z[3]
- dz4=Z[1]-Z[3]+0.5*impulse(x)
-
- return np.array([dz1,dz2,dz3,dz4])
+# Exact solution by using odeint
+F = lambda Z,t: [Z[1],Z[3] - Z[1] - Z[0],Z[3],impulse(t)/2 + Z[1] - Z[3]] 
+# Defined the function
+# There is a -0.5 be ignored because the constant disappears after differentiation.
 
 
 '''
@@ -94,18 +121,22 @@ def ode(t,u): #odeint - the exact solution
     return dzdt
 
 '''
+'''
+Step 3: Solve the function 
+'''
 
-F = lambda Z,t: [Z[1],Z[3] - Z[1] - Z[0],Z[3],impulse(t)/2 + Z[1] - Z[3]]
 Eu_result= Euler(state_matrix,x)  # z is the Euler method solution
 He_result= heun(state_matrix,x)   # w is the Heun method solution
-Exact = integrate.odeint(F,[0,0,0,0],x) # exact solution
-
+Exact = integrate.odeint(F,[0,0,0,0],x) #solve the function
 
 exact=np.zeros(len(x))     # Extract the third variables - X2(t)
 for i in range(len(x)-1):
  exact[i]=Exact[i][2]
  i+=1
 
+'''
+Step4: error analysis
+'''
 
 Error_euler=np.zeros(len(x))   #error array initialize
 Error_Heun=np.zeros(len(x))
@@ -119,6 +150,10 @@ for i in range(len(x)-1):  #save the error and print the result
  Error_Heun[i]=error(He_result[i],exact[i])
  print(x[i],"\t",exact[i],"\t",Eu_result[i],"\t ",error(Eu_result[i],exact[i]),"\t",He_result[i],error(He_result[i],exact[i]),"\t",end="\t")
  print()
+
+'''
+Step5 : Plot the figure
+'''
 
 # plot the simulation
 plt.plot(x,Eu_result,'--',label='Euler')
